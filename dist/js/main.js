1a5a714917669db8d86c8f391b1c4426ec898030
Mentikora <notender_boy@ukr.net>
2018-05-21 01:37:30 +0300

    welcome page, top10 ins, jquery, slick.js, directions list, consultation form

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,14 @@
-document.addEventListener("DOMContentLoaded",function(){});
+document.addEventListener("DOMContentLoaded",function(){
+
+  $('.top-educational-institutions-slider').slick({
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow: '<button class="slider-arrow slider-arrow--prev icon-ar-left"></button>',
+    nextArrow: '<button class="slider-arrow slider-arrow--next icon-ar-right"></button>',
+    dots: true,
+    dotsClass: 'slider-dots'
+  });
+
+});
