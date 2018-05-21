47194c111537840daf797b64e731c2dd460a2f21
Mentikora <notender_boy@ukr.net>
2018-05-22 02:00:34 +0300

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,8 @@
-document.addEventListener("DOMContentLoaded",function(){
+document.addEventListener("DOMContentLoaded", function(){
 
   'use strict';
 
+  // institutes slider
   $('.top-educational-institutions-slider').slick({
     infinite: false,
     speed: 300,
@@ -10,7 +11,58 @@ document.addEventListener("DOMContentLoaded",function(){
     prevArrow: '<button class="slider-arrow slider-arrow--prev icon-ar-left"></button>',
     nextArrow: '<button class="slider-arrow slider-arrow--next icon-ar-right"></button>',
     dots: true,
-    dotsClass: 'slider-dots'
+    dotsClass: 'slider-dots',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  });
+
+  // camps slider
+  $('.camps-row').slick({
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: false,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          dots: true,
+          dotsClass: 'slider-dots',
+          arrows: true,
+          prevArrow: '<button class="slider-arrow slider-arrow--prev icon-ar-left"></button>',
+          nextArrow: '<button class="slider-arrow slider-arrow--next icon-ar-right"></button>',
+          variableWidth: true,
+          centerMode: true,
+          centerPadding: '60px',
+        }
+      },
+      {
+        breakpoint: 568,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+          dotsClass: 'slider-dots',
+          arrows: true,
+          prevArrow: '<button class="slider-arrow slider-arrow--prev icon-ar-left"></button>',
+          nextArrow: '<button class="slider-arrow slider-arrow--next icon-ar-right"></button>',
+          variableWidth: true,
+          centerMode: true,
+          centerPadding: '60px',
+        }
+      }
+    ]
   });
 
 });
